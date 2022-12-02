73872d14abf15ccdcb35a2eee36d43c634616bdb
Jack Shannon <j.b.shannon@lse.ac.uk>
2022-12-02 15:26:04 +0000

    fixed missing `score` in Day 2

--- calendar/02/solution.jl
+++ calendar/02/solution.jl
@@ -1,8 +1,9 @@
 rounds = readlines("input.txt")
 
-decode(round) = (mod(Int(round[1]), Int('A')), mod(Int(round[3]), Int('X')))
+decode(round) = (round[1] - 'A', round[3] - 'X')
 shoot(me, opponent) = mod(1 + mod(me - opponent, 3), 3)
 strategy(outcome, opponent) = mod(opponent + (outcome-1), 3)
+score(me, outcome) = 1 + me + 3*outcome
 
 function score1(round)
     opponent, me = decode(round)
